54664ef0ed34efcb14f7d3d3f605934968a741de
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-27 13:41:33 -0400

    Imported components inside of the App.js file

--- showcase/src/App.js
+++ showcase/src/App.js
@@ -1,8 +1,17 @@
+import Button from './components/button/Button'
+import Download from './components/download/Download'
+import Features from './components/features/Features'
+import SectionWrapper from './components/sectionwrapper/SectionWrapper'
+
 
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">React Native NFT Showcase</h1>
+      <Button />
+      <Download />
+      <Features />
+      <SectionWrapper />
     </div>
   );
 }
