4d1e62a91dae6250f50abbc7beb804798028ab9e
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-28 13:59:39 -0400

    Added dynamic styling to the first three divs inside of the SectionWrapper component

--- showcase/src/App.js
+++ showcase/src/App.js
@@ -2,6 +2,7 @@ import Download from './components/download/Download'
 import Features from './components/features/Features'
 import SectionWrapper from './components/sectionwrapper/SectionWrapper'
 import home from './assets/home_hero.png'
+import homeCard from './assets/home_cards.png'
 
 
 
@@ -9,10 +10,15 @@ function App() {
   return (
     <>
       <SectionWrapper title="Your own store of NFTs. Start Selling and Growing"
-        description="Buy, store, collect NFTs, exchange and earn Crypto. Join 25+ million people"
+        description="Buy, store, collect NFTs, exchange and earn Crypto. Join 25+ million people."
         showBtn
         mockupImg={home}
         banner="banner"
+      />
+      <SectionWrapper title="Smart User Interface Marketplace"
+        description="Experience an awesome UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI design."
+        mockupImg={homeCard}
+        reverse
       />
     </>
   );
