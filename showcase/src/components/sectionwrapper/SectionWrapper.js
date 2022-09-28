410123f9f99bb550d1b7eef8b8d342fd3d6a5f11
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-28 13:48:14 -0400

    Changing the size of the mockup image based on the screen size

--- showcase/src/components/sectionwrapper/SectionWrapper.js
+++ showcase/src/components/sectionwrapper/SectionWrapper.js
@@ -6,16 +6,17 @@ import expo from '../../assets/expo.png'
 const SectionWrapper = ({ title, description, showBtn, mockupImg, banner }) => {
   return (
     <div className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}>
-      <div className={`flex items-center ${styles.boxClass}`}>
+      <div className={`flex items-center ${styles.boxClass} w-11/12 sm:w-full minmd:3/4`}>
         <div className={`${styles.descDiv} fadeLeftMini`}>
           <h1 className={`${styles.h1Text}`}>{title}</h1>
           <p className={`${styles.descriptionText}`}>{description}</p>
           {showBtn && <Button assetUrl={expo} link="deployed nft marketplace" />}
         </div>
+        <div className={`flex-1 ${styles.flexCenter} p-8 sm:px-0`}>
+          <img className={`${styles.sectionImg}`} src={mockupImg} alt='mockup' />
+
+        </div>
       </div>
-      <h2>{title}</h2>
-      <h5>{banner}</h5>
-      {/* <img src={mockupImg} alt='mockup' /> */}
     </div>
   )
 }
