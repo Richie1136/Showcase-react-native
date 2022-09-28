49f357e6157d87b48065f5d60c01041ed975f2af
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-28 13:35:41 -0400

    Added styling to html elements and used props inside of the Button component

--- showcase/src/components/button/Button.js
+++ showcase/src/components/button/Button.js
@@ -1,7 +1,15 @@
+import styles from "../../styles/Global"
+
 
 const Button = ({ assetUrl, link }) => {
   return (
-    <div>Button</div>
+    <div className={`${styles.btnBlack}`} onClick={() => window.open(link, "_blank")}>
+      <img className={`${styles.btnIcon}`} src={assetUrl} alt='assets' />
+      <div className="flex flex-col justify-start ml-4">
+        <p className={`${styles.btnText} font-normal text-xs`}>View it on</p>
+        <p className={`${styles.btnText} font-bold text-sm`}>Expo Store</p>
+      </div>
+    </div>
   )
 }
 
