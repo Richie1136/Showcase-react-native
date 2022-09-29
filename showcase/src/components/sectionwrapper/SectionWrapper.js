2f4c5fcdec023a209fd4b451a9784987a38792ad
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-29 13:12:27 -0400

    Changing the h1 text color based on if the background is white or not

--- showcase/src/components/sectionwrapper/SectionWrapper.js
+++ showcase/src/components/sectionwrapper/SectionWrapper.js
@@ -8,7 +8,7 @@ const SectionWrapper = ({ title, description, showBtn, mockupImg, banner, revers
     <div className={`min-h-screen ${styles.section} ${reverse ? styles.bgWhite : styles.bgPrimary} ${banner}`}>
       <div className={`flex items-center ${reverse ? styles.boxReverseClass : styles.boxClass} w-11/12 sm:w-full minmd:3/4`}>
         <div className={`${styles.descDiv} ${reverse ? " fadeRightMini" : " fadeLeftMini"} ${reverse ? styles.textRight : styles.textLeft}`}>
-          <h1 className={`${styles.h1Text}`}>{title}</h1>
+          <h1 className={` ${reverse ? styles.blackText : styles.whiteText} ${styles.h1Text}`}>{title}</h1>
           <p className={`${styles.descriptionText}`}>{description}</p>
           {showBtn && <Button assetUrl={expo} link="deployed nft marketplace" />}
         </div>
