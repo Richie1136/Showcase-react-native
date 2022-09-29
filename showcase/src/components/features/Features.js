f94424f871f6c669efcf73f95d4f78d4a269aeb8
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-29 13:28:48 -0400

    Added styling to the h1 and p tag inside of the Features component

--- showcase/src/components/features/Features.js
+++ showcase/src/components/features/Features.js
@@ -3,7 +3,14 @@ import styles from "../../styles/Global"
 
 const Features = () => {
   return (
-    <div className={`${styles.section} ${styles.bgPrimary} banner03`}></div>
+    <div className={`${styles.section} ${styles.bgPrimary} banner03`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div>
+          <h1 className={`${styles.h1Text} ${styles.whiteText}`}>Technologies</h1>
+          <p className={`${styles.h1Text} ${styles.whiteText}`}>ProNef has been developed using a cross-platform technology, React Native.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
