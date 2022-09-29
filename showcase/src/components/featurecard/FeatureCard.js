1cb85fee5f0455368ae9b1eb2d447cbfef1ee826
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-29 13:40:37 -0400

    Rendering out the the image of each featureCard inside of the Features component

--- showcase/src/components/featurecard/FeatureCard.js
+++ showcase/src/components/featurecard/FeatureCard.js
@@ -2,7 +2,9 @@ import styles from "../../styles/Global"
 
 
 const FeatureCard = ({ iconUrl, iconText }) => (
-  <div className={styles.featureCard}>{iconText}</div>
+  <div className={styles.featureCard}>
+    <img className={`${styles.featureImg}`} src={iconUrl} alt='icon' />
+  </div>
 )
 
 export default FeatureCard
