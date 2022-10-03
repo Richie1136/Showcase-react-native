668fef21bd5c4bd1489309b90073adc9a7feee73
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-03 13:19:59 -0400

    Linking my github source code

--- showcase/src/components/download/Download.js
+++ showcase/src/components/download/Download.js
@@ -7,8 +7,9 @@ const Download = () => {
       <div className={`${styles.subSection} flex-col text-center`}>
         <div>
           <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the Source Code</h1>
-          <p>Get the full source code on GitHub</p>
+          <p className={`${styles.pText} ${styles.pText}`}>Get the full source code on GitHub</p>
         </div>
+        <button className={`${styles.btnPrimary}`}><a href="https://github.com/Richie1136/Showcase-react-native/tree/master/showcase/src" target="_blank" rel="noreferrer">Source Code</a></button>
       </div>
     </div>
   )
