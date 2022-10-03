c86a205cc3a23a931023360de57f9f69643b362a
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-03 13:23:32 -0400

    Added an image below the button

--- showcase/src/components/download/Download.js
+++ showcase/src/components/download/Download.js
@@ -1,4 +1,5 @@
 import styles from "../../styles/Global"
+import scene from '../../assets/scene.png'
 
 
 const Download = () => {
@@ -10,6 +11,9 @@ const Download = () => {
           <p className={`${styles.pText} ${styles.pText}`}>Get the full source code on GitHub</p>
         </div>
         <button className={`${styles.btnPrimary}`}><a href="https://github.com/Richie1136/Showcase-react-native/tree/master/showcase/src" target="_blank" rel="noreferrer">Source Code</a></button>
+        <div className={`${styles.flexCenter}`}>
+          <img src={scene} alt="download png" className={`${styles.fullImg}`} />
+        </div>
       </div>
     </div>
   )
