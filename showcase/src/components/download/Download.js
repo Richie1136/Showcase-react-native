d96088db458d138f56165e039abd26d5671c9fd9
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-03 13:10:41 -0400

    Adding styling to my Download component and building out the component

--- showcase/src/components/download/Download.js
+++ showcase/src/components/download/Download.js
@@ -3,7 +3,14 @@ import styles from "../../styles/Global"
 
 const Download = () => {
   return (
-    <div>Download</div>
+    <div className={`${styles.section} ${styles.bgWhite}`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div>
+          <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the Source Code</h1>
+          <p>Get the full source code on GitHub</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
